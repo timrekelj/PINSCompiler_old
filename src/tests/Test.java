a29091c46d798f4f1d666678946b4567ff0b4631
Tim Rekelj <tim.rekelj@proton.me>
2023-03-01 17:23:35 +0100

    added better tests and short documentation

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -5,27 +5,54 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import ArgPar.Annotation.ParsableCommand;
+import ArgPar.Annotation.ParsableOption;
+import ArgPar.Exception.ParseException;
+import ArgPar.Parser.ArgumentParser;
 import compiler.lexer.Lexer;
 
+@ParsableCommand(commandName = "TEST", description = "Tester for the PINS compiler")
 public class Test {
+
+    @ParsableOption(name = "--folder")
+    public String folder = "";
+
+    /**
+     * Get phase that we want to test from args
+     */
+    public static Test parse(String[] args) {
+        try {
+            var parser = new ArgumentParser<Test>(Test.class);
+            return parser.parse(args);
+        } catch (ParseException __) {
+            System.exit(2);
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
 
+        
+        var cli = Test.parse(args);
+        
         // get number of test files
-        String[] tests = getTestNames();
+        String[] tests = getTestNames(cli.folder);
 
-        // delete all .err files inside Tests/LEX
-        deleteErrFiles();
+        // delete all .err files inside folder
+        deleteErrFiles(cli.folder);
 
         // run tests
-        runTest(tests);
+        runTest(tests, cli.folder);
     }
 
     /**
      * Counts the number of all files inside test folder
+     * @param folder folder name from args
      * @return names of test files
      */
-    private static String[] getTestNames() {
-        String[] allFiles = new File("src/tests/LEX").list();
+    private static String[] getTestNames(String folder) {
+        String[] allFiles = new File("src/tests/" + folder).list();
         var tests = new ArrayList<String>();
 
         for (String file : allFiles) {
@@ -38,13 +65,14 @@ public class Test {
     }
 
     /**
-     * Deletes all .err files inside tests/LEX
+     * Deletes all .err files inside folder
      * Minimizes the confusion when comparing files
+     * @param folder folder name from args
      */
-    private static void deleteErrFiles() {
-        // delete all .err files inside tests/LEX
-        File folder = new File("src/tests/LEX");
-        File[] listOfFiles = folder.listFiles();
+    private static void deleteErrFiles(String folder) {
+        // delete all .err files inside folder
+        File deleteDir = new File("src/tests/" + folder);
+        File[] listOfFiles = deleteDir.listFiles();
         for (File file : listOfFiles) {
             if (file.isFile() && file.getName().endsWith(".err")) {
                 file.delete();
@@ -54,24 +82,25 @@ public class Test {
 
     /**
      * Runs all tests
-     * @param numberOfTests number of tests inside current folder
+     * @param tests names of tests in folder (without extensions)
+     * @param folder folder name from args
      */
-    private static void runTest(String[] tests) {
+    private static void runTest(String[] tests, String folder) {
         String output = "";
         String expected = "";
         double countOK = 0;
 
         for (int i = 0; i < tests.length; i++) {
-            output = getOutput(tests[i]);
-            expected = readFile("src/tests/LEX/" + tests[i] + ".out");
+            output = getOutput(tests[i], folder);
+            expected = readFile("src/tests/" + folder + "/" + tests[i] + ".out");
             System.out.print("Test " + tests[i] + ": ");
             if (output.equals(expected)) {
                 countOK++;
                 System.out.println("OK");
             } else {
                 System.out.println("FAIL");
-                System.out.println("Compare files: src/tests/LEX/" + tests[i] + ".out and src/tests/LEX/" + tests[i] + ".err");
-                writeErrFile(output, tests[i]);
+                System.out.println("Compare files: src/tests/" + folder + "/" + tests[i] + ".out and src/tests/" + folder + "/" + tests[i] + ".err");
+                writeErrFile(output, tests[i], folder);
             }
         }
 
@@ -81,11 +110,12 @@ public class Test {
 
     /**
      * Gets program output for a given test
-     * @param testNumber ID number of the test
+     * @param test name of the test
+     * @param folder folder name from args
      * @return output produced by the program
      */
-    private static String getOutput(String test) {
-        String input = readFile("src/tests/LEX/" + test + ".pins");
+    private static String getOutput(String test, String folder) {
+        String input = readFile("src/tests/" + folder + "/" + test + ".pins");
         var symbols = new Lexer(input).scan();
         StringBuilder output = new StringBuilder();
         for (var symbol : symbols) {
@@ -120,11 +150,12 @@ public class Test {
      * Writes output to a file.
      * Only happens for test that failed.
      * @param output output produced by the program
-     * @param testNum ID number of the test
+     * @param test name of the test
+     * @param folder folder name from args
      */
-    private static void writeErrFile(String output, String test) {
+    private static void writeErrFile(String output, String test, String folder) {
         try {
-            File file = new File("src/tests/LEX/" + test + ".err");
+            File file = new File("src/tests/" + folder + "/" + test + ".err");
             file.createNewFile();
             FileWriter writer = new FileWriter(file);
             writer.write(output.strip());
@@ -134,4 +165,5 @@ public class Test {
             System.exit(1);
         }
     }
+
 }
